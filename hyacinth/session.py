c7fb6347f042ea50b320e29b3fa3237ef695615d
Anson MacKeracher <anson@legalmate.co>
2023-01-17 16:16:00 -0500

    L-292: Support file uploads (#5)
    * Adds POST, PUT, PATCH
    
    * Adds fix for fields in upload_document
    
    * Debug info
    
    * Fixes for document upload
    
    * More fixes for document upload
    
    * Fixes for document_upload put
    
    * POWER YOGA
    
    * Janky... we probably dont want to auto-decode json anyway
    
    * debug
    
    * Debug
    
    * Don't use authenticated HTTP client for upload
    
    * Clean up debug

--- hyacinth/session.py
+++ hyacinth/session.py
@@ -1,3 +1,4 @@
+import requests
 from authlib.integrations.requests_client import OAuth2Session
 
 CLIO_API_BASE_URL_US="https://app.clio.com/api/v4"
@@ -9,14 +10,14 @@ class Session:
                                      token=token)
 
     @staticmethod
-    def make_url(path):
+    def __make_url(path):
         return f"{CLIO_API_BASE_URL_US}/{path}.json"
 
-    def get_resource(self, url, **kwargs):
+    def __get_resource(self, url, **kwargs):
         resp = self.session.get(url, params=kwargs)
         return resp.json()
 
-    def get_paginated_resource(self, url, **kwargs):
+    def __get_paginated_resource(self, url, **kwargs):
         next_url = url
         while next_url:
             resource = self.get_resource(next_url, **kwargs)
@@ -36,38 +37,100 @@ class Session:
                 # no paging meta, break the loop
                 next_url = None
 
+    def __post_resource(self, url, json, **kwargs):
+        resp = self.session.post(url, json=json, **kwargs)
+        return resp.json()
+
+    def __patch_resource(self, url, json, **kwargs):
+        resp = self.session.patch(url, json=json, **kwargs)
+        return resp.json()
+
     def get_contact(self, id, **kwargs):
-        url = Session.make_url(f"contacts/{id}")
-        return self.get_resource(url, **kwargs)
+        """GET a Contact."""
+        url = Session.__make_url(f"contacts/{id}")
+        return self.__get_resource(url, **kwargs)
 
     def get_contacts(self, **kwargs):
-        url = Session.make_url("contacts")
-        return self.get_paginated_resource(url, **kwargs)
+        """GET a list of Contacts."""
+        url = Session.__make_url("contacts")
+        return self.__get_paginated_resource(url, **kwargs)
 
     def get_who_am_i(self, **kwargs):
-        url = Session.make_url("users/who_am_i")
-        return self.get_resource(url, **kwargs)
+        """GET currently authenticated User."""
+        url = Session.__make_url("users/who_am_i")
+        return self.__get_resource(url, **kwargs)
 
     def get_user(self, id, **kwargs):
-        url = Session.make_url(f"users/{id}")
-        return self.get_resource(url, **kwargs)
+        """GET a single User."""
+        url = Session.__make_url(f"users/{id}")
+        return self.__get_resource(url, **kwargs)
 
     def get_users(self, **kwargs):
-        url = Session.make_url("users")
-        return self.get_paginated_resource(url, **kwargs)
+        """GET a list of Users."""
+        url = Session.__make_url("users")
+        return self.__get_paginated_resource(url, **kwargs)
 
     def get_document(self, id, **kwargs):
-        url = Session.make_url(f"documents/{id}")
-        return self.get_resource(url, **kwargs)
+        """GET a Document."""
+        url = Session.__make_url(f"documents/{id}")
+        return self.__get_resource(url, **kwargs)
 
     def get_documents(self, **kwargs):
-        url = Session.make_url("documents")
-        return self.get_paginated_resource(url, **kwargs)
+        """GET a list of Documents."""
+        url = Session.__make_url("documents")
+        return self.__get_paginated_resource(url, **kwargs)
 
     def get_matter(self, id, **kwargs):
-        url = Session.make_url(f"matters/{id}")
-        return self.get_resource(url, **kwargs)
+        """GET a Matter."""
+        url = Session.__make_url(f"matters/{id}")
+        return self.__get_resource(url, **kwargs)
 
     def get_matters(self, **kwargs):
-        url = Session.make_url("matters")
-        return self.get_paginated_resource(url, **kwargs)
+        """GET a list of Matters."""
+        url = Session.__make_url("matters")
+        return self.__get_paginated_resource(url, **kwargs)
+
+    def upload_document(self, name, parent_id, parent_type, document):
+        """POST a new Document, PUT the data, and PATCH Document as fully_uploaded."""
+        post_url = Session.__make_url("documents")
+        clio_document = self.__post_resource(
+            post_url,
+            params={"fields": "id,latest_document_version{uuid,put_url,put_headers}"},
+            json={
+                "data": {
+                    "name": name,
+                    "parent": {
+                        "id": parent_id,
+                        "type": parent_type
+                    }
+                }
+            }
+        )
+
+        put_url = clio_document["data"]["latest_document_version"]["put_url"]
+        put_headers = clio_document["data"]["latest_document_version"]["put_headers"]
+
+        headers_map = {}
+        for header in put_headers:
+            headers_map[header['name']] = header['value']
+
+        # We actually DON'T want to use the authenticated client here
+        requests.put(
+            put_url,
+            headers=headers_map,
+            data=document
+        )
+
+        patch_url = self.__make_url(f"documents/{clio_document['data']['id']}")
+        patch_resp = self.__patch_resource(
+            patch_url,
+            params={"fields": "id,name,latest_document_version{fully_uploaded}"},
+            json={
+                "data": {
+                    "uuid": clio_document["data"]["latest_document_version"]["uuid"],
+                    "fully_uploaded": True
+                }
+            }
+        )
+
+        return patch_resp
