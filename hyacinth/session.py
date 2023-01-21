d0c3a7126391502e7b93c3fb15bfd591d79e710a
Anson MacKeracher <anson@legalmate.co>
2023-01-21 08:52:43 -0500

    don't decode empty responses

--- hyacinth/session.py
+++ hyacinth/session.py
@@ -28,8 +28,13 @@ def ratelimit(f):
             resp.raise_for_status()
 
         self.update_ratelimits(resp)
-        print(f"content: {resp.content}")
+
+        # DELETE responses have no content
+        if not resp.content:
+            return None
+
         return resp.json()
+
     return wrapper
 
 
