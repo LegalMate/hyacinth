46508e3831399d7e85ec689dc47aa4b160689f99
Anson MacKeracher <anson@legalmate.co>
2023-02-05 19:54:19 -0500

    GET calendar entry details and list

--- hyacinth/session.py
+++ hyacinth/session.py
@@ -111,6 +111,16 @@ class Session:
         url = Session.__make_url("calendar_entries")
         return self.__post_resource(url, json, **kwargs)
 
+    def get_calendar_entries(self, **kwargs):
+        """GET a list of Calendar Entries."""
+        url = Session.__make_url("calendar_entries")
+        return self.__get_paginated_resource(url, **kwargs)
+
+    def get_calendar_entry(self, calendar_entry_id, **kwargs):
+        """GET a Calendar Entry with provided ID."""
+        url = Session.__make_url(f"calendar_entries/{calendar_entry_id}")
+        return self.__get_resource(url, **kwargs)
+
     def get_contact(self, id, **kwargs):
         """GET a Contact with provided ID."""
         url = Session.__make_url(f"contacts/{id}")
