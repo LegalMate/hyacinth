b3930f21530e71d3e7add5f2651076db68b33111
Anson MacKeracher <anson@legalmate.co>
2023-04-17 07:43:36 -0400

    Add WIP

--- hyacinth/session.py
+++ hyacinth/session.py
@@ -55,7 +55,7 @@ class Session:
     ):
         """Initialize Session with optional ratelimits."""
         self.session = OAuth2Session(
-            client_id=client_id, client_secret=client_secret, token=token
+            client_id=client_id, client_secret=client_secret, token=token, token_endpoint=TODO
         )
 
         self.ratelimit = ratelimit
