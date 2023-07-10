4058869ff4a9146172153c34e3f1c68292c9bb99
Anson MacKeracher <anson@legalmate.co>
2023-07-10 12:04:40 +0100

    Fix test_delete_webhook

--- test/test_session.py
+++ test/test_session.py
@@ -187,5 +187,5 @@ class TestSession(unittest.TestCase):
         self.session._Session__delete_resource = MagicMock()
         _ = self.session.delete_webhook(1)
         self.session._Session__delete_resource.assert_called_with(
-            "https://app.clio.com/api/v4/webhooks/1"
+            "https://app.clio.com/api/v4/webhooks/1.json"
         )
