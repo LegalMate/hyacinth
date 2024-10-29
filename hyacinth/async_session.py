873b59457cb0317f69f7acaf36ffd400ca7e7d54
Nick <nicksfrancis@gmail.com>
2024-10-29 21:03:10 +0000

    Bring over more from the sync client to async

--- hyacinth/async_session.py
+++ hyacinth/async_session.py
@@ -379,3 +379,393 @@ class AsyncSession:
         """DELETE an existing Task Template List with provided ID."""
         url = self.make_url(f"task_template_lists/{id}")
         return await self.delete_resource(url, **kwargs)
+
+    async def get_matters(self, **kwargs):
+        """GET a list of Matters."""
+        url = self.make_url("matters")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_folder(self, id, **kwargs):
+        """GET a Folder with provided ID."""
+        url = self.make_url(f"folders/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def get_folders(self, **kwargs):
+        """GET a list of Folders."""
+        url = self.make_url("folders")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_folders_content(self, **kwargs):
+        """GET a list of Folder contents."""
+        url = self.make_url("folders/list")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def post_folder(self, name, parent_id, parent_type, **kwargs):
+        """POST a new Folder."""
+        url = self.make_url("folders")
+        return await self.post_resource(
+            url,
+            json={
+                "data": {"name": name, "parent": {"id": parent_id, "type": parent_type}}
+            },
+            **kwargs,
+        )
+
+    async def delete_folder(self, id, **kwargs):
+        """DELETE an existing Folder with provided ID."""
+        url = self.make_url(f"folders/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def upload_multipart_document(
+        self,
+        name,
+        parent_id,
+        parent_type,
+        document,
+        progress_update,
+        document_category_id=None,
+    ):
+        """Async fn to upload a Document to Clio via the multipart upload feature."""
+        async with aiofiles.open(document, "rb") as f:
+            file_size = await aiofiles.os.path.getsize(document)
+            parts = []
+            for i in range(0, file_size, PART_SIZE):
+                start_offset = i
+                end_offset = min(file_size, i + PART_SIZE)
+                part = await f.read(end_offset - start_offset)
+                parts.append((start_offset, end_offset, part))
+                progress_update()
+            # content_md5 = hashlib.md5(part).digest()  # nosec
+            # content_md5_str = base64.b64encode(content_md5).decode('utf-8')
+
+        multiparts = []
+        for idx, part in enumerate(parts, start=1):
+            multiparts.append(
+                {
+                    "part_number": idx,
+                    "content_length": len(part[2]),
+                    # "content_md5": content_md5_str,
+                }
+            )
+
+        post_url = self.make_url("documents")
+
+        clio_document = await self.post_resource(
+            post_url,
+            params={
+                "fields": "id,latest_document_version{uuid,put_headers,multiparts}"
+            },
+            json={
+                "data": {
+                    "name": name,
+                    "parent": {"id": parent_id, "type": parent_type},
+                    "document_category": {"id": document_category_id},
+                    "multiparts": multiparts,
+                }
+            },
+        )
+
+        for part in clio_document["data"]["latest_document_version"]["multiparts"]:
+            put_url = part["put_url"]
+            put_headers = part["put_headers"]
+            headers_map = {}
+            for header in put_headers:
+                headers_map[header["name"]] = header["value"]
+
+            part_number = part["part_number"]
+            data_part = parts[part_number - 1][2]  # 'parts' is a list of tuples
+            async with aiohttp.ClientSession() as session:
+                response = await session.put(
+                    put_url, headers=headers_map, data=data_part, timeout=300
+                )
+                log.info(response)
+                progress_update()
+
+        patch_url = self.make_url(f"documents/{clio_document['data']['id']}")
+        patch_resp = await self.patch_resource(
+            patch_url,
+            params={"fields": "id,name,latest_document_version{fully_uploaded}"},
+            json={
+                "data": {
+                    "uuid": clio_document["data"]["latest_document_version"]["uuid"],
+                    "fully_uploaded": True,
+                }
+            },
+        )
+        return patch_resp
+
+    async def post_webhook(self, url, model, events, fields=None, expires_at=None):
+        """Post a Webhook to Clio."""
+        post_url = self.make_url("webhooks")
+        model_fields = "id"
+        if fields:
+            model_fields += "," + fields
+        return await self.post_resource(
+            post_url,
+            json={
+                "data": {
+                    "fields": model_fields,
+                    "events": events,
+                    "model": model,
+                    "url": url,
+                    "expires_at": expires_at,
+                },
+            },
+            params={"fields": "id,shared_secret,status,expires_at"},
+        )
+
+    async def update_webhook(self, id, json, **kwargs):
+        """PATCH an existing Webhook with provided ID."""
+        url = self.make_url(f"webhooks/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_webhook(self, id, **kwargs):
+        """DELETE an existing Webhook with provided ID."""
+        url = self.make_url(f"webhooks/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def get_webhook(self, id, **kwargs):
+        """GET a single Webhook with provided ID."""
+        url = self.make_url(f"webhooks/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def get_webhooks(self, **kwargs):
+        """GET a list of all webhooks from Clio."""
+        url = self.make_url("webhooks")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_document_templates(self, **kwargs):
+        """GET a list of Document Templates."""
+        url = self.make_url("document_templates")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_document_automation(self, document_automation_id, **kwargs):
+        """GET a Document Automation."""
+        url = self.make_url(f"document_automations/{document_automation_id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def post_document_automation(self, json, **kwargs):
+        """POST a new Document Automation."""
+        url = self.make_url("document_automations")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def post_matter(self, json, **kwargs):
+        """POST a new Matter."""
+        url = self.make_url("matters")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def get_activity(self, id, **kwargs):
+        """GET a single Activity with provided ID."""
+        url = self.make_url(f"activities/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def get_activities(self, **kwargs):
+        """GET a list of Activities."""
+        url = self.make_url("activities")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def post_activity(self, json, **kwargs):
+        """POST a new Activity."""
+        url = self.make_url("activities")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_activity(self, id, json, **kwargs):
+        """PATCH an existing Activity with provided ID."""
+        url = self.make_url(f"activities/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_activity(self, id, **kwargs):
+        """DELETE an existing Activity with provided ID."""
+        url = self.make_url(f"activities/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def get_activity_description(self, id, **kwargs):
+        """GET a single Activity Description with provided ID."""
+        url = self.make_url(f"activity_descriptions/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def get_activity_descriptions(self, **kwargs):
+        """GET a list of Activity Descriptions."""
+        url = self.make_url("activity_descriptions")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def post_activity_description(self, json, **kwargs):
+        """POST a new Activity Description."""
+        url = self.make_url("activity_descriptions")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def get_bills(self, **kwargs):
+        """GET a list of Bills."""
+        url = self.make_url("bills")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_bill(self, id, **kwargs):
+        """GET a single Bill with provided ID."""
+        url = self.make_url(f"bills/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def patch_bill(self, id, json, **kwargs):
+        """PATCH an existing Bill with provided ID."""
+        url = self.make_url(f"bills/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_bill(self, id, **kwargs):
+        """DELETE an existing Bill with provided ID."""
+        url = self.make_url(f"bills/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def get_line_items(self, **kwargs):
+        """GET a list of Line Items."""
+        url = self.make_url("line_items")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def patch_line_item(self, id, json, **kwargs):
+        """PATCH an existing line item with provided ID of line item."""
+        url = self.make_url(f"line_items/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def post_trust_request(self, json, **kwargs):
+        """POST a new Trust Request."""
+        url = self.make_url("trust_requests")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def get_custom_actions(self, **kwargs):
+        """GET a list of Custom Actions."""
+        url = self.make_url("custom_actions")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_custom_action(self, id, **kwargs):
+        """GET a single Custom Action with provided ID."""
+        url = self.make_url(f"custom_actions/{id}")
+        return self.get_resource(url, **kwargs)
+
+    async def post_custom_action(self, json, **kwargs):
+        """POST a new Custom Action."""
+        url = self.make_url("custom_actions")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_custom_action(self, id, json, **kwargs):
+        """PATCH an existing Custom Action with provided ID."""
+        url = self.make_url(f"custom_actions/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_custom_action(self, id, **kwargs):
+        """DELETE an existing Custom Action with provided ID."""
+        url = self.make_url(f"custom_actions/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def verify_custom_action(self, subject_url, custom_action_nonce, **kwargs):
+        """Verify a Custom Action."""
+        url = self.base_url + subject_url
+        params = kwargs.get("params", {})
+        params.update({"custom_action_nonce": custom_action_nonce})
+        kwargs["params"] = params
+        return await self.get_resource(
+            url,
+            **kwargs,
+        )
+
+    async def get_relationships(self, **kwargs):
+        """GET a list of Relationships."""
+        url = self.make_url("relationships")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_relationship(self, id, **kwargs):
+        """GET a single Relationship with provided ID."""
+        url = self.make_url(f"relationships/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def post_relationship(self, json, **kwargs):
+        """POST a new Relationship."""
+        url = self.make_url("relationships")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_relationship(self, id, json, **kwargs):
+        """PATCH an existing Relationship with provided ID."""
+        url = self.make_url(f"relationships/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_relationship(self, id, **kwargs):
+        """DELETE an existing Relationship with provided ID."""
+        url = self.make_url(f"relationships/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def post_document_category(self, json, **kwargs):
+        """POST a new Document Category."""
+        url = self.make_url("document_categories")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def get_practice_areas(self, **kwargs):
+        """GET a list of Practice Areas."""
+        url = self.make_url("practice_areas")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_practice_area(self, id, **kwargs):
+        """GET a single Practice Area with provided ID."""
+        url = self.make_url(f"practice_areas/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def post_practice_area(self, json, **kwargs):
+        """POST a new Practice Area."""
+        url = self.make_url("practice_areas")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_practice_area(self, id, json, **kwargs):
+        """PATCH an existing Practice Area with provided ID."""
+        url = self.make_url(f"practice_areas/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_practice_area(self, id, **kwargs):
+        """DELETE an existing Practice Area with provided ID."""
+        url = self.make_url(f"practice_areas/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def get_communications(self, **kwargs):
+        """GET a list of Communications."""
+        url = self.make_url("communications")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_communication(self, id, **kwargs):
+        """GET a single Communication with provided ID."""
+        url = self.make_url(f"communications/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def post_communication(self, json, **kwargs):
+        """POST a new Communication."""
+        url = self.make_url("communications")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_communication(self, id, json, **kwargs):
+        """PATCH an existing Communication with provided ID."""
+        url = self.make_url(f"communications/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_communication(self, id, **kwargs):
+        """DELETE an existing Communication with provided ID."""
+        url = self.make_url(f"communications/{id}")
+        return await self.delete_resource(url, **kwargs)
+
+    async def get_reminders(self, **kwargs):
+        """GET a list of Reminders."""
+        url = self.make_url("reminders")
+        return await self.get_paginated_resource(url, **kwargs)
+
+    async def get_reminder(self, id, **kwargs):
+        """GET a single Reminder with provided ID."""
+        url = self.make_url(f"reminders/{id}")
+        return await self.get_resource(url, **kwargs)
+
+    async def post_reminder(self, json, **kwargs):
+        """POST a new Reminder."""
+        url = self.make_url("reminders")
+        return await self.post_resource(url, json=json, **kwargs)
+
+    async def patch_reminder(self, id, json, **kwargs):
+        """PATCH an existing Reminder with provided ID."""
+        url = self.make_url(f"reminders/{id}")
+        return await self.patch_resource(url, json=json, **kwargs)
+
+    async def delete_reminder(self, id, **kwargs):
+        """DELETE an existing Reminder with provided ID."""
+        url = self.make_url(f"reminders/{id}")
+        return await self.delete_resource(url, **kwargs)
